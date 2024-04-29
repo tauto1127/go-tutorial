eaa43ddd4d3557df97524c0897072dbfcdf3a7b2
takuto <tiharu717@gmail.com>
2024-04-29 13:52:01 +0900

    add Entity

---
+++ go-clean-architecture-tutorial/Entity/product.go
@@ -0,0 +1,20 @@
+package core
+
+import "errors"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
+func (p *Product) UpdateName(name string) error {
+	if len(name) == 0 {
+		// エラー分はキャピタライずしちゃだめ？
+		return errors.New("Name is required")
+	}
+	p.Name = name
+	return nil
+}
